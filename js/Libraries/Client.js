af8d359134eaf6a3566385ff8c8f4acb62582a20
jealbertus <jealbertus@hotmail.com>
2026-01-01 15:09:30 -0600

    Add new Library Client

---
+++ js/Libraries/Client.js
@@ -0,0 +1,30 @@
+export class Client{
+ 
+
+	constructor(
+		id				,
+		parentID		,
+		model			,
+		entity			,
+		filter			,
+		data			,
+		type			,
+		cardinality 	,
+		namespace   	
+	){
+		this.id				= 	id				
+		this.parentID		= 	parentID		
+		this.model			= 	model			
+		this.entity			= 	entity			
+		this.filter			= 	filter			
+		this.data			= 	data			
+		this.type			= 	type			
+		this.cardinality 	= 	cardinality 	
+		this.namespace 	    = 	namespace 	    
+	}
+
+
+
+	
+	
+}
